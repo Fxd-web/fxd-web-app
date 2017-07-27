be00d83ec0158bcebd172d21a300fd80e5994f30
gxx <562606139@qq.com>
2017-07-27 09:50:43 +0800

    7.27-gxx

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app" @click="tap_console">
+  <div id="app">
       <!--<transition :name="transitionName">-->
         <!--<keep-alive>-->
         <router-view class="view"></router-view>
@@ -19,6 +19,7 @@
   import 'normalize.css';
   import './css/common.scss';
   import nav from './components/nav';
+  import vconsole from './mixins/vconsole';
 
   export default {
     name: 'app',
@@ -26,15 +27,14 @@
       return {
         transitionName: 'slide-left',
         navSwitch: false,
-        vconsole: null,
       };
     },
+    mixins: [vconsole],
     components: {
       'fxd-nav': nav,
     },
     mounted() {
       this.$nextTick(() => {
-        this.vconsole_init();
         this.interceptLogin();
         if (~['', 'home', 'me', 'more'].indexOf(this.$route.path.slice(1))) { // eslint-disable-line no-bitwise
           this.navSwitch = true;
@@ -58,37 +58,8 @@
       },
     },
     methods: {
-      /**
-       * 初始化加载vconsole，如果是生产默认则不加载
-       */
-      vconsole_init() {
-        if (process.env.NODE_ENV === 'development') {
-          this.vconsole = require('vconsole');
-        }
-      },
       /* eslint-disable */
-      /***
-       * 不知道什么原因vconsole初始化之后获取不到dom元素，所以只能通过点击屏幕来唤醒vconsole实例
-       * 默认meta标签里面的缩放大小为0.5，所以显得vconsole会缩小一半，通过flex(100, 1, 1)将缩放比例
-       * 扩大到1，显示正常。之后的取消操作再将缩放比例还原。
-       */
-      tap_console() {
-        try {
-          if (this.vconsole.isReady) {
-            this.vconsole.$.bind(document.querySelector('.vc-switch'), 'click', function() {
-              flex(100, 1, 1);
-            });
-            this.vconsole.$.bind(document.querySelector('.vc-hide'), 'click', function() {
-              flex(100, 1);
-            });
-            this.vconsole.$.bind(document.querySelector('.vc-mask'), 'click', function() {
-              flex(100, 1);
-            });
-          }
-        } catch (e) {}
-      },
       interceptLogin() { // 登录拦截
-
 //        if (this.$route.path === '/login') {
 //          return;
 //        }
---
+++ src/mixins/vconsole.js
@@ -0,0 +1,57 @@
+/**
+ * Created by gxx on 2017/7/27.
+ */
+export default {
+  data(){
+    return{
+      vconsole: null,
+    }
+  },
+  mounted() {
+    this.$nextTick(() => {
+      this.init_vconsole();
+      setTimeout(()=> {
+        document.getElementById('app').click();
+        this.tap_console();
+      },0)
+    });
+  },
+  methods:{
+    /**
+     * 初始化加载vconsole，如果是生产默认则不加载
+     */
+    init_vconsole() {
+      if (process.env.NODE_ENV === 'development') {
+        this.vconsole = require('vconsole');
+
+      }
+    },
+    /***
+     * 不知道什么原因vconsole初始化之后获取不到dom元素，所以只能通过点击屏幕来唤醒vconsole实例
+     * 默认meta标签里面的缩放大小为0.5，所以显得vconsole会缩小一半，通过flex(100, 1, 1)将缩放比例
+     * 扩大到1，显示正常。之后的取消操作再将缩放比例还原。
+     */
+    tap_console() {
+      try {
+        if (this.vconsole.isReady) {
+          this.vconsole.$.bind(document.querySelector('.vc-switch'), 'click', function() {
+            flex(100, 1, 1);
+          });
+          this.vconsole.$.bind(document.querySelector('.vc-hide'), 'click', function() {
+            flex(100, 1);
+          });
+          this.vconsole.$.bind(document.querySelector('.vc-mask'), 'click', function() {
+            flex(100, 1);
+          });
+        }
+      } catch (e) {}
+    },
+  },
+};
+
+
+
+
+
+
+
